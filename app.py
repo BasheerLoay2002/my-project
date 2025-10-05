ade257168dbf0c5645c04ce1f38e4fc406c1bcc0
Basheer Loay <basheer.loay@harri.com>
2025-10-05 16:07:45 +0300

    Add files

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Simple Python Application
+"""
+
+def main():
+    print("Hello from my Python app!")
+    print("Version: 1.0.0")
+    print("Running successfully!")
+
+if __name__ == "__main__":
+    main()
